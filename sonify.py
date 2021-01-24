7c97bfcc21589af425d0e71b869ec8ddf3396bb3
Lara Arikan <larisarikan@gmail.com>
2021-01-23 23:41:52 -0800

    Add text functionality

--- sonify.py
+++ sonify.py
@@ -11,6 +11,7 @@
 
 from midiutil.MidiFile import MIDIFile
 import numpy as np
+import re
 
 # Error functions
 
@@ -71,6 +72,8 @@ def compress(data: np.array, to_mean: int):
         result = np.vstack([result, lastAvg])
         
     return result
+
+# -----------------------------------------------------------------------------
 
 # Float sonification
 
@@ -356,3 +359,116 @@ def matrix(data: np.array, maxf: int, minf: int,
     with open(filename, "wb") as output_file:
         midi.writeFile(output_file)
 
+# -----------------------------------------------------------------------------
+
+# Text sonification
+
+# All stopwords: Pruning these allows greater discrimination between texts. 
+# Culled from https://programminghistorian.org/en/lessons/counting-frequencies 
+
+stopwords = ['a', 'about', 'above', 'across', 'after', 'afterwards']
+stopwords += ['again', 'against', 'all', 'almost', 'alone', 'along']
+stopwords += ['already', 'also', 'although', 'always', 'am', 'among']
+stopwords += ['amongst', 'amoungst', 'amount', 'an', 'and', 'another']
+stopwords += ['any', 'anyhow', 'anyone', 'anything', 'anyway', 'anywhere']
+stopwords += ['are', 'around', 'as', 'at', 'back', 'be', 'became']
+stopwords += ['because', 'become', 'becomes', 'becoming', 'been']
+stopwords += ['before', 'beforehand', 'behind', 'being', 'below']
+stopwords += ['beside', 'besides', 'between', 'beyond', 'bill', 'both']
+stopwords += ['bottom', 'but', 'by', 'call', 'can', 'cannot', 'cant']
+stopwords += ['co', 'computer', 'con', 'could', 'couldnt', 'cry', 'de']
+stopwords += ['describe', 'detail', 'did', 'do', 'done', 'down', 'due']
+stopwords += ['during', 'each', 'eg', 'eight', 'either', 'eleven', 'else']
+stopwords += ['elsewhere', 'empty', 'enough', 'etc', 'even', 'ever']
+stopwords += ['every', 'everyone', 'everything', 'everywhere', 'except']
+stopwords += ['few', 'fifteen', 'fifty', 'fill', 'find', 'fire', 'first']
+stopwords += ['five', 'for', 'former', 'formerly', 'forty', 'found']
+stopwords += ['four', 'from', 'front', 'full', 'further', 'get', 'give']
+stopwords += ['go', 'had', 'has', 'hasnt', 'have', 'he', 'hence', 'her']
+stopwords += ['here', 'hereafter', 'hereby', 'herein', 'hereupon', 'hers']
+stopwords += ['herself', 'him', 'himself', 'his', 'how', 'however']
+stopwords += ['hundred', 'i', 'ie', 'if', 'in', 'inc', 'indeed']
+stopwords += ['interest', 'into', 'is', 'it', 'its', 'itself', 'keep']
+stopwords += ['last', 'latter', 'latterly', 'least', 'less', 'ltd', 'made']
+stopwords += ['many', 'may', 'me', 'meanwhile', 'might', 'mill', 'mine']
+stopwords += ['more', 'moreover', 'most', 'mostly', 'move', 'much']
+stopwords += ['must', 'my', 'myself', 'name', 'namely', 'neither', 'never']
+stopwords += ['nevertheless', 'next', 'nine', 'no', 'nobody', 'none']
+stopwords += ['noone', 'nor', 'not', 'nothing', 'now', 'nowhere', 'of']
+stopwords += ['off', 'often', 'on','once', 'one', 'only', 'onto', 'or']
+stopwords += ['other', 'others', 'otherwise', 'our', 'ours', 'ourselves']
+stopwords += ['out', 'over', 'own', 'part', 'per', 'perhaps', 'please']
+stopwords += ['put', 'rather', 're', 's', 'same', 'see', 'seem', 'seemed']
+stopwords += ['seeming', 'seems', 'serious', 'several', 'she', 'should']
+stopwords += ['show', 'side', 'since', 'sincere', 'six', 'sixty', 'so']
+stopwords += ['some', 'somehow', 'someone', 'something', 'sometime']
+stopwords += ['sometimes', 'somewhere', 'still', 'such', 'system', 'take']
+stopwords += ['ten', 'than', 'that', 'the', 'their', 'them', 'themselves']
+stopwords += ['then', 'thence', 'there', 'thereafter', 'thereby']
+stopwords += ['therefore', 'therein', 'thereupon', 'these', 'they']
+stopwords += ['thick', 'thin', 'third', 'this', 'those', 'though', 'three']
+stopwords += ['three', 'through', 'throughout', 'thru', 'thus', 'to']
+stopwords += ['together', 'too', 'top', 'toward', 'towards', 'twelve']
+stopwords += ['twenty', 'two', 'un', 'under', 'until', 'up', 'upon']
+stopwords += ['us', 'very', 'via', 'was', 'we', 'well', 'were', 'what']
+stopwords += ['whatever', 'when', 'whence', 'whenever', 'where']
+stopwords += ['whereafter', 'whereas', 'whereby', 'wherein', 'whereupon']
+stopwords += ['wherever', 'whether', 'which', 'while', 'whither', 'who']
+stopwords += ['whoever', 'whole', 'whom', 'whose', 'why', 'will', 'with']
+stopwords += ['within', 'without', 'would', 'yet', 'you', 'your']
+stopwords += ['yours', 'yourself', 'yourselves']
+
+# Standardize string for counting
+
+def process(s: str):  
+    s = re.sub(r'[^\w\s]', '', s) # remove punctuation
+    return s.lower() # make string lowercase
+
+# Build dictionary of word to frequency
+
+def freqdict(tokens: list):
+    wordfreq = {}
+    
+    for token in tokens:
+        wordfreq[token] = tokens.count(token)
+        
+    return wordfreq
+
+def freqtext(text: str, maxf: int, minf: int, 
+             stops = stopwords, 
+             tempo = 120, volume = 90, duration = 0.25, filename = 'sonified.mid', 
+             below_bound = None, below_volume = 90, below_duration = 0.25, 
+             above_bound = None, above_volume = 90, above_duration = 0.25,
+             equal_val = None, equal_volume = 90, equal_duration = 0.25, 
+             interval = None, int_pitch = 50, int_volume = 0, int_duration = 0.5):
+    
+    """
+    This function takes in text as a string, tokenizes it, then replaces each token
+    with the frequency of its appearance in the text, which produces a 1D array of ints.
+    This can be sonified using floats, documented above.
+    
+    For long texts like stories or articles, it will pay to strip away what are known
+    as stopwords: words that appear often in language regardless of context, like pronouns
+    or prepositions. This helps discriminate between texts of different subject matter.
+    The user can supply their own list of stopwords, or set this variable to None if they
+    have a shorter string they'd like not to prune.
+    
+    :param stops: a list of words that the user does not want to include in the frequency
+    count; by default this is a standard list of stopwords.
+
+    """
+    tokens = [process(s) for s in text.split()]
+    
+    if stops is not None:
+        tokens = [word for word in tokens if word not in stops]
+    
+    wordfreq = freqdict(tokens)
+    
+    freqs = np.array([wordfreq[word] for word in tokens])
+        
+    return floats(freqs, maxf, minf, 
+           tempo, volume, duration, filename, 
+           below_bound, below_volume, below_duration, 
+           above_bound, above_volume, above_duration,
+           equal_val, equal_volume, equal_duration, 
+           interval, int_pitch, int_volume, int_duration)
